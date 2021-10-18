31637d8d37b2e76bb4826686985fdd7e8df5c383
JulijaKu <julia47@inbox.lv>
2021-10-18 19:24:11 +0300

    Task

--- src/selenium/sample/Sample3Task.java
+++ src/selenium/sample/Sample3Task.java
@@ -1,6 +1,7 @@
 package selenium.sample;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -38,13 +39,23 @@ public class Sample3Task {
     @Test
     public void assertEqualsTask() throws Exception {
 //         TODO:
+
 //         check how many element with class "test" there are on page (5)
+        int expected = 5;
+        int actual = driver.findElements(By.className("test")).size();
+        assertEquals(expected, actual);
 //         check that value of second button is "This is also a button"
+        String expected2 = "This is also a button";
+        String actual2 = driver.findElement(By.id("buttonId")).getAttribute("value");
+        assertEquals(expected2, actual2);
     }
 
     @Test
     public void assertTrueTask() throws Exception {
 //         TODO:
+        String expected = driver.findElement(By.id("buttonId")).getAttribute("value");
+        assertTrue(expected.equalsIgnoreCase("this is Also a Button"));
+
 //         check that it is True that value of second button is
 //         "this is Also a Button" if you ignore Caps Locks
 //         fail with custom error message:
@@ -53,7 +64,11 @@ public class Sample3Task {
     @Test
     public void assertFalseTask() throws Exception {
 //         TODO:
+
 //        check that it is False that value of second button is "This is a button"
+        String expected = driver.findElement(By.id("buttonId")).getAttribute("value");
+        System.out.println(expected);
+        assertFalse(expected.equalsIgnoreCase("This is a button"));
     }
 
     @Test
@@ -61,5 +76,11 @@ public class Sample3Task {
 //        TODO:
 //        check that none of items with class "test"
 //        contain number 190
+        List<WebElement> allElementsWithClass = driver.findElements(By.className("test"));
+        for (WebElement elementWithClass : allElementsWithClass) {
+            assertFalse(elementWithClass.getText().contains("190"));
+            System.out.println(elementWithClass.getText());
+
+        }
     }
 }
